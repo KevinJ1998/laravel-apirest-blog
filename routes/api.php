5b7186d108c5d022ba8886f6fdf41a54e045b668
Kevin Segovia <kjsegovia144@hotmail.es>
2020-04-22 12:02:52 -0500

    #6 Controlador para article - Vinculacion implicita entre modelo y ruta

--- routes/api.php
+++ routes/api.php
@@ -19,21 +19,8 @@ use Illuminate\Support\Facades\Route;
 //    return $request->user();
 //});
 
-Route::get('articles', function () {
-    return Article::all();
-});
-Route::get('articles/{id}', function ( $id ) {
-    return Article::find( $id );
-});
-Route::post('articles', function ( Request $request ) {
-    return Article::create( $request -> all() );
-});
-Route::put('articles/{id}', function ( Request $request, $id ) {
-    $article = Article::findOrFail( $id );
-    $article -> update( $request -> all() );
-    return $article;
-});
-Route::delete('articles/{id}', function ($id) {
-    Article::find( $id ) -> delete();
-    return 204;
-});
+Route::get('articles', 'ArticleControler@index');
+Route::get('articles/{article}', 'ArticleControler@show');
+Route::post('articles', 'ArticleControler@store');
+Route::put('articles/{article}', 'ArticleControler@update');
+Route::delete('articles/{article}', 'ArticleControler@delete');
