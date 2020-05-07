97ebe8dc61dcf15f294b53cf478dbe8d86f5e919
Kevin Segovia <kjsegovia144@hotmail.es>
2020-05-06 19:33:32 -0500

    Tabala para relacion muchos a muchos entre categoria y usuario

--- app/Article.php
+++ app/Article.php
@@ -3,16 +3,27 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Article extends Model
 {
-    protected $fillable = ['title','body'];
+    protected $fillable = ['title', 'body'];
 
-    public function comments() {
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($article) {
+            $article->user_id = Auth::id();
+        });
+    }
+
+    public function comments()
+    {
         return $this->hasMany('App\Comment');
     }
 
-    public function user() {
+    public function user()
+    {
         return $this->belongsTo('App\User');
     }
 }
