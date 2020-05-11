3b5d81cf403c2e066767fc283e711af0742df5d9
Kevin Segovia <kjsegovia144@hotmail.es>
2020-05-10 23:16:37 -0500

    Recursos de la api para articles y users

---
+++ app/Http/Resources/Article.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\User;
+use App\Category;
+
+class Article extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'body' => $this->body,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'user' => '/api/user/' - $this->user_id,
+            'category' => '/api/category/' - $this->category_id
+        ];
+    }
+}
