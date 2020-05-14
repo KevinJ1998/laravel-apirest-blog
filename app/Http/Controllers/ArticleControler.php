6ab999c77278e3640f83e34b521828193e653a6e
Kevin Segovia <kjsegovia144@hotmail.es>
2020-05-13 20:07:15 -0500

    perimisos y rolÃes mediante politicas

--- app/Http/Controllers/ArticleControler.php
+++ app/Http/Controllers/ArticleControler.php
@@ -18,16 +18,19 @@ class ArticleControler extends Controller
 
     public function index()
     {
+        // $this->authorize('viewAny', Article::class);
         return new ArticleCollection(Article::paginate());
     }
 
     public function show(Article $article)
     {
+        $this->authorize('view', $article);
         return response()->json(new ArticleResource($article), 200);
     }
 
     public function store(Request $request)
     {
+        $this->authorize('create', Article::class);
 
         $request->validate([
             'title' => 'required|string|unique:articles|max:255',
@@ -46,6 +49,7 @@ class ArticleControler extends Controller
 
     public function update(Request $request, Article $article)
     {
+        $this->authorize('update', $article);
         $request->validate([
             'title' => 'required|string|unique:articles,title,'.$article->id.'|max:255',
             'body' => 'required',
--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use phpDocumentor\Reflection\Types\Self_;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
@@ -38,6 +39,16 @@ class User extends Authenticatable implements JWTSubject
         'email_verified_at' => 'datetime',
     ];
 
+    const ROLE_SUPERADMIN = 'ROLE_SUPERADMIN';
+    const ROLE_ADMIN = 'ROLE_ADMIN';
+    const ROLE_USER = 'ROLE_USER';
+
+    private const ROLES_HIERARCHY = [
+        self::ROLE_SUPERADMIN => [self::ROLE_ADMIN],
+        self::ROLE_ADMIN => [self::ROLE_USER],
+        self::ROLE_USER => []
+    ];
+
     public function getJWTIdentifier()
     {
         return $this->getKey();
@@ -48,15 +59,41 @@ class User extends Authenticatable implements JWTSubject
         return [];
     }
 
-    public function articles() {
+    public function articles()
+    {
         return $this->hasMany('App\Article');
     }
 
-    public function comments() {
+    public function comments()
+    {
         return $this->hasMany('App\Comment');
     }
 
-    public function categories() {
+    public function categories()
+    {
         return $this->belongsToMany('App\Category')->as('subscriptions')->withTimestamps();
+    }
+
+    public function isGranted($role)
+    {
+        if ($role === $this->role) {
+            return true;
+        }
+        return self::isRoleInHierarchy($role, self::ROLES_HIERARCHY[$this->role]);
+    }
+
+    private static function isRoleInHierarchy($role, $role_hierarchy)
+    {
+        if (in_array($role, $role_hierarchy)) {
+            return true;
+        }
+
+        foreach ($role_hierarchy as $role_included) {
+            if (self::isRoleInHierarchy($role, self::ROLES_HIERARCHY[$role_included])) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
